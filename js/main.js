505ecd3d026f0875aff363429f6e90c52a0b76c3
maribeltorresg <maribel.torres.guevara@gmail.commaribegit config --global user.email maribel.torres.guevara@gmail.com>
2018-12-21 00:58:53 -0500

    Integrando un plugin de jQuery para la sección de Cuenta Regresiva

--- js/main.js
+++ js/main.js
@@ -182,4 +182,12 @@ $(document).ready(function() {
     $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1300);
     $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1500);
 
+    // Cuenta regresiva
+    $('.cuenta-regresiva').countdown('2018/12/25 11:00:00', function(event) {
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+
 });
