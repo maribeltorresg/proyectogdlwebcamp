c9ac1475e76524f085836f283ea25f8eb7a05ac7
maribeltorresg <maribel.torres.guevara@gmail.commaribegit config --global user.email maribel.torres.guevara@gmail.com>
2018-12-21 00:36:47 -0500

    Integrando un plugin de jQuery para la sección de Contador

--- js/main.js
+++ js/main.js
@@ -175,4 +175,11 @@ $(document).ready(function() {
 
         return false;
     });
+
+    // Animaciones para los numeros (contadores)
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1500);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1300);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1500);
+
 });
